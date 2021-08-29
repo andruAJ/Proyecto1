f89a2437ebba23816a5a606eff1cb2da11d75a0e
Andrés Juan <andres.giraldov@upb.edu.co>
2021-08-28 22:15:03 -0500

    void config

--- Proyecto1.ino
+++ Proyecto1.ino
@@ -2,9 +2,25 @@ int Counter = 20;
 void config()
 {
  Serial.println(Counter);
+  if (digitalRead(13)== HIGH)
+  {
+    Counter++;
+  }
+  if (digitalRead(12)== HIGH)
+  {
+    Counter--;
+  }
+  if (digitalRead(10)== HIGH
+  {
+      countDown();
+  }
 }
+void countDown(); 
 void setup()
 {
+  pinMode(13, INPUT);
+  pinMode(12, INPUT);
+  pinMode(10, INPUT);
  Serial.begin(115200);
  config();
 }
